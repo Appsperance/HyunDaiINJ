6c0d29205acfc15f50a74c8b53f7c6a6b8b0a167
한민기 <hmk904@naver.com>
2025-01-02 14:15:06 +0900

    MQTT WPF 테스트 완료

---
+++ Dto/RabbitMQQualityZeroDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HyunDaiINJ.Dto
+{
+    public class RabbitMQQualityZeroDTO
+    {
+        [JsonProperty("productName")]
+        public string? ProductName { get; set; }
+
+        [JsonProperty("lotNumber")]
+        public string? LotNumber { get; set; }
+
+        [JsonProperty("productId")]
+        public string? ProductId { get; set; }
+
+        [JsonProperty("lineId")]
+        public string? LineId { get; set; }
+
+        [JsonProperty("operatorName")]
+        public string? OperatorName { get; set; }
+    }
+}
---
+++ Models/MQTTService.cs
@@ -0,0 +1,88 @@
+﻿using MQTTnet.Client;
+using MQTTnet;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using HyunDaiINJ.Dto;
+using System.IO;
+
+public class MqttService
+{
+    private IMqttClient? _mqttClient;
+
+    public event Action<string, RabbitMQQualityOneDTO>? OnQualityOneMessageReceived;
+    public event Action<string, RabbitMQQualityZeroDTO>? OnQualityZeroMessageReceived;
+
+    public async Task ConnectAsync(string brokerAddress, string username, string password)
+    {
+        var factory = new MqttFactory();
+        _mqttClient = factory.CreateMqttClient();
+
+        var options = new MqttClientOptionsBuilder()
+            .WithTcpServer(brokerAddress, 1883)
+            .WithCredentials(username, password)
+            .WithCleanSession(true)
+            .Build();
+
+        Console.WriteLine($"[MqttService] MQTT 브로커 연결 시도: {brokerAddress}, 사용자: {username}");
+
+        _mqttClient.ApplicationMessageReceivedAsync += e =>
+        {
+            var topic = e.ApplicationMessage.Topic;
+            var payload = Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment.ToArray());
+
+            Console.WriteLine($"[MqttService] 수신된 메시지 - 토픽: {topic}, 내용: {payload}");
+
+            // 수신된 메시지 저장 (디버깅)
+            File.WriteAllText($"C:\\temp\\mqtt_message_{DateTime.Now.Ticks}.json", payload);
+            Console.WriteLine($"MQTT 메시지 저장 완료: C:\\temp\\mqtt_message_{DateTime.Now.Ticks}.json");
+
+            try
+            {
+                if (topic.StartsWith("Quality/one"))
+                {
+                    var qualityOneDto = JsonConvert.DeserializeObject<RabbitMQQualityOneDTO>(payload);
+                    OnQualityOneMessageReceived?.Invoke(topic, qualityOneDto);
+                }
+                else if (topic.StartsWith("Quality/zero"))
+                {
+                    var qualityZeroDto = JsonConvert.DeserializeObject<RabbitMQQualityZeroDTO>(payload);
+                    OnQualityZeroMessageReceived?.Invoke(topic, qualityZeroDto);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[MqttService] 역직렬화 실패: {ex.Message}");
+            }
+
+            return Task.CompletedTask;
+        };
+
+
+        try
+        {
+            await _mqttClient.ConnectAsync(options);
+            Console.WriteLine("[MqttService] MQTT 연결 성공");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[MqttService] MQTT 연결 실패: {ex.Message}");
+        }
+    }
+
+    public async Task SubscribeAsync(string topic)
+    {
+        if (_mqttClient.IsConnected)
+        {
+            await _mqttClient.SubscribeAsync(new MqttTopicFilterBuilder()
+                .WithTopic(topic)
+                .Build());
+            Console.WriteLine($"[MqttService] 토픽 구독 성공: {topic}");
+        }
+        else
+        {
+            Console.WriteLine("[MqttService] MQTT 연결이 활성화되지 않았습니다.");
+        }
+    }
+}
