7ffaa69ffba406bf44db48ee1e3aeaa567dbf84e
한민기 <hmk904@naver.com>
2025-01-04 23:49:50 +0900

    MQTT View , ViewModel , Model , DTO 수정

---
+++ Dto/MQTTDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HyunDaiINJ.DTO
+{
+    public class MQTTDTO
+    {
+        [JsonProperty("lineId")]
+        public string? LineId { get; set; }
+
+        [JsonProperty("lotId")]
+        public string? LotId { get; set; }
+
+        [JsonProperty("shift")]
+        public string? Shift { get; set; }
+
+        [JsonProperty("employeeNumber")]
+        public string? EmployeeNumber { get; set; }
+
+        [JsonProperty("ngImg")]
+        public byte[]? NgImg { get; set; }
+
+        [JsonProperty("stageVal")]
+        public string? StageVal { get; set; }
+
+    }
+
+}
--- Views/MQTT/MQTTTEST.xaml.cs
+++ Views/MQTT/MQTTTEST.xaml.cs
@@ -8,37 +8,16 @@ namespace HyunDaiINJ.Views.MQTT
 {
     public partial class MQTTTEST : Page
     {
-        private readonly MqttViewModel _viewModel;
-
         public MQTTTEST()
         {
             InitializeComponent();
-            _viewModel = new MqttViewModel();
-            DataContext = _viewModel;
-        }
 
-        private async void ConnectAndSubscribeButton_Click(object sender, RoutedEventArgs e)
-        {
-            string brokerAddress = "43.203.159.137";
-            string username = "admin";
-            string password = "vapor";
+            var mqttModel = new MQTTModel();
 
-            try
-            {
-                await _viewModel.ConnectAndSubscribeAsync(brokerAddress, username, password);
-                if (_viewModel.IsConnected)
-                {
-                    MessageBox.Show("MQTT 연결 성공!", "정보", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-                else
-                {
-                    MessageBox.Show("MQTT 연결 실패!", "오류", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"연결 중 오류 발생: {ex.Message}", "오류", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            var mqttViewModel = new MqttViewModel(mqttModel);
+
+            DataContext = mqttViewModel;
+
         }
     }
 }
