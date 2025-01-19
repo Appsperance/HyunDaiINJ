ddae0f17523859ec4eb8af87c171f705bbe15fea
한민기 <hmk904@naver.com>
2025-01-19 20:20:31 +0900

    시간 수정

--- Views/MainView.xaml.cs
+++ Views/MainView.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace HyunDaiINJ.Views
 {
@@ -21,10 +22,25 @@ namespace HyunDaiINJ.Views
     /// </summary>
     public partial class MainView : Window
     {
+
+        private DispatcherTimer _timer;
+
         public MainView()
         {
             InitializeComponent();
             //this.DataContext = new MainViewModel();
+
+            // 만약 '현재시간'만 표시하면 되므로, 굳이 DataContext는 안 써도 됨.
+            // _timer 설정
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            _timer.Tick += (s, e) =>
+            {
+                TxtCurrentTime.Text = DateTime.Now.ToString("yyyy-MM-dd (ddd) HH시 mm분 ss초");
+            };
+            _timer.Start();
         }
     }
 }
--- Views/Monitoring/Pages/Monitoring/ProcessMonitoring.xaml.cs
+++ Views/Monitoring/Pages/Monitoring/ProcessMonitoring.xaml.cs
@@ -7,23 +7,9 @@ namespace HyunDaiINJ.Views.Monitoring.Pages.Monitoring
 {
     public partial class ProcessMonitoring : Page
     {
-        private readonly DispatcherTimer _timer;
-
         public ProcessMonitoring()
         {
             InitializeComponent();
-
-            //// 만약 '현재시간'만 표시하면 되므로, 굳이 DataContext는 안 써도 됨.
-            //// _timer 설정
-            //_timer = new DispatcherTimer
-            //{
-            //    Interval = TimeSpan.FromSeconds(1)
-            //};
-            //_timer.Tick += (s, e) =>
-            //{
-            //    TxtCurrentTime.Text = DateTime.Now.ToString("yyyy-MM-dd (ddd) HH시 mm분 ss초");
-            //};
-            //_timer.Start();
         }
     }
 }
